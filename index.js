7b091031bbb50d15e6a21a0693de1c1a2a1cae42
Nic Jackson <jackson.nic@gmail.com>
2021-02-09 15:30:01 +0000

    handle multiple languages

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const emoji = require('node-emoji')
 
-const app = express()
-app.use(bodyParser.text())
-
 const projectId = process.env.GOOGLE_PROJECT
 
 // Imports the Google Cloud client library
@@ -13,47 +10,6 @@ const { Translate } = require('@google-cloud/translate').v2
 
 // Instantiates a client
 const translate = new Translate({ projectId })
-
-// respond with "hello world" when a GET request is made to the homepage
-app.post('/translate', function (req, res) {
-  // The text to translate
-  const text = req.body
-
-  // The target language
-  const target = 'ru'
-
-  console.log(`handle translate request lang: ${target} text: ${text}`)
-
-  // first translate into english
-  const translation = translate.translate(text, 'en')
-  translation.then((d) => {
-    console.log(`Translation: ${d[0]}`)
-    const emojiText = addEmojiToText(text)
-
-    // now translate into russian
-    const translation = translate.translate(emojiText, target)
-    translation.then((d) => {
-      console.log(`Translation: ${d[0]}`)
-      res.send(d[0])
-    }).catch((e) => {
-      console.log(`Error: ${e}`)
-      res.status(e, 500)
-    })
-  }).catch((e) => {
-    console.log(`Error: ${e}`)
-    res.status(e, 500)
-  })
-})
-
-app.get('/health', function (req, res) {
-  console.log('handle health request', emoji.get('heart'))
-
-  res.send(emoji.get('heart'))
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
 
 const addEmojiToText = function (text) {
   const words = text.split(' ')
@@ -70,3 +26,80 @@ const addEmojiToText = function (text) {
 
   return emoji.emojify(returnString.join(' '))
 }
+
+let langs = []
+
+const fetchLanguages = async function () {
+  // Lists available translation language with their names in English (the default).
+  const [languages] = await translate.getLanguages()
+  console.log('Loaded languages:', languages)
+
+  langs = languages
+}
+
+// fetch the languages
+fetchLanguages()
+
+const app = express()
+app.use(bodyParser.text())
+
+// respond with "hello world" when a GET request is made to the homepage
+app.post('/translate/:lang', function (req, res) {
+  // The text to translate
+  const text = req.body
+
+  // The target language
+  const target = req.params.lang
+
+  console.log(`handle translate request lang: ${target} text: ${text}`)
+
+  // validate the language
+  const ok = langs.some(l => {
+    return (l.code === target)
+  })
+
+  if (!ok) {
+    console.log(ok)
+    const message = `Invalid language: ${target}`
+    console.log(message)
+
+    res.status(402).send(message)
+    return
+  }
+
+  // first translate into english
+  const translation = translate.translate(text, 'en')
+  translation.then((d) => {
+    console.log(`Translation: ${d[0]}`)
+    const emojiText = addEmojiToText(text)
+
+    // now translate into russian
+    const translation = translate.translate(emojiText, target)
+    translation.then((d) => {
+      console.log(`Translation: ${d[0]}`)
+      res.send(d[0])
+    }).catch((e) => {
+      console.log(`Error: ${e}`)
+      res.status(500).send(e)
+    })
+  }).catch((e) => {
+    console.log(`Error: ${e}`)
+    res.status(500).send(e)
+  })
+})
+
+app.get('/health', function (req, res) {
+  console.log('handle health request', emoji.get('heart'))
+
+  res.send(emoji.get('heart'))
+})
+
+app.get('/languages', function (req, res) {
+  console.log('handle languages')
+
+  res.send(langs)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
